d004cd5dd6ffe09ddc1d2db739e910c72886691a
DevElexson <elexson.melo@gmail.com>
2022-06-28 18:19:55 -0300

    Quarta Alteração

--- Projeto1/Program.cs
+++ Projeto1/Program.cs
@@ -6,7 +6,73 @@ namespace GFT_Teste1
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Construindo matriz....");
 
+            Console.WriteLine("Qual será o numero de linhas?");
+
+            var linhas = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual será o numero de colunas?");
+
+            var colunas = Int32.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[linhas, colunas];
+
+
+            for (var coluna = 0; coluna < colunas; coluna++)
+            {
+                for (var linha = 0; linha < linhas; linha++)
+                {
+                    System.Console.WriteLine($"Qual será o valor da célula de posicão {linha}:{coluna}?");
+                    matriz[linha, coluna] = Int32.Parse(Console.ReadLine());
+                }
+            }
+
+            for (var coluna = 0; coluna < colunas; coluna++)
+            {
+                for (var linha = 0; linha < linhas; linha++)
+                {
+                    System.Console.WriteLine($"");
+
+                    System.Console.WriteLine($"Valores ao redor da célula de posicão {linha}:{coluna}");
+                    try
+                    {
+                        var celulaTeste = matriz[linha - 1, coluna];
+                        System.Console.WriteLine($"Célula esquerda: {celulaTeste}");
+                    }
+                    catch (System.Exception ex)
+                    {
+                        System.Console.WriteLine("Não tem nenhum valor à esquerda.");
+                    }
+                    try
+                    {
+                        var celulaTeste = matriz[linha + 1, coluna];
+                        System.Console.WriteLine($"Célula direita: {celulaTeste}");
+                    }
+                    catch (System.Exception ex)
+                    {
+                        System.Console.WriteLine("Não tem nenhum valor à direita.");
+                    }
+                    try
+                    {
+                        var celulaTeste = matriz[linha, coluna - 1];
+                        System.Console.WriteLine($"Célula acima: {celulaTeste}");
+                    }
+                    catch (System.Exception ex)
+                    {
+                        System.Console.WriteLine("Não tem nenhum valor acima.");
+                    }
+                    try
+                    {
+                        var celulaTeste = matriz[linha, coluna + 1];
+                        System.Console.WriteLine($"Célula abaixo: {celulaTeste}");
+                    }
+                    catch (System.Exception ex)
+                    {
+                        System.Console.WriteLine("Não tem nenhum valor abaixo.");
+                    }
+                }
+            }
         }
     }
 }
--- Projeto3/Program.cs
+++ Projeto3/Program.cs
@@ -7,6 +7,54 @@ namespace GFT_Teste3
         public static void Main(string[] args)
         {
 
+            var trabalhadores = new List<Trabalhador>();
+
+            var trabalhador1 = new Trabalhador("Joao", 10, 40);
+            trabalhadores.Add(trabalhador1);
+
+            var trabalhador2 = new Trabalhador("Cleber", 15, 30);
+            trabalhadores.Add(trabalhador2);
+
+            var trabalhador3 = new Trabalhador("Cleiton", 17, 50);
+            trabalhadores.Add(trabalhador3);
+
+            var trabalhador4 = new Trabalhador("Matheus", 8, 80);
+            trabalhadores.Add(trabalhador4);
+
+            var trabalhador5 = new Trabalhador("Jorge", 30, 10);
+            trabalhadores.Add(trabalhador5);
+
+
+            Console.WriteLine("Olá, temos os seguintes trabalhadores registrados: ");
+
+            int i = 1;
+
+            foreach (var trabalhador in trabalhadores)
+            {
+                System.Console.WriteLine($"{i}- Nome: {trabalhador.Nome}");
+                i++;
+            }
+
+
+            int opcaoUsuario = 0;
+
+            do
+            {
+                Console.WriteLine("Qual trabalhador você deseja consultar?");
+
+                opcaoUsuario = Int32.Parse(Console.ReadLine());
+
+                if (opcaoUsuario < 1 || opcaoUsuario > i) Console.WriteLine("Opção inválida. Tente novamente.");
+
+
+            } while (opcaoUsuario < 1 || opcaoUsuario > i);
+
+
+            var trabalhadorConsultado = trabalhadores[opcaoUsuario - 1];
+
+            Console.WriteLine($"Trabalhador consultado: {trabalhadorConsultado.Nome} | Salário por Hora: R${trabalhadorConsultado.SalarioHora} | Horas trabalhadas: {trabalhadorConsultado.HorasTrabalhadas}");
+
+            Console.WriteLine($"Salário calculado: R${trabalhadorConsultado.SalarioTotal}");
         }
     }
 }
