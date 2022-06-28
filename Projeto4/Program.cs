2394874a20b14d64d915e52668d6353d2e534be9
DevElexson <elexson.melo@gmail.com>
2022-06-28 18:01:01 -0300

    Terceira Alteracao

--- Projeto4/Program.cs
+++ Projeto4/Program.cs
@@ -6,7 +6,24 @@ namespace GFT_Teste4
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Digite um número entre 3 e 999 para verificar seus números primos: ");
+            int n = int.Parse(Console.ReadLine());
+            bool primo = true;
 
+            while (n <= 3 && n >= 999)
+            {
+                Console.WriteLine("Número inválido, digite novamente");
+                Console.WriteLine("Digite um número entre 3 e 999 para verificar seus números primos: ");
+                n = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 2; i < n; i++)
+            {
+                if (i % 2 == 0 && i / i == 1)
+                {
+                    Console.Write("Números primos: " + i + " ");
+                }
+            }
         }
     }
 }
